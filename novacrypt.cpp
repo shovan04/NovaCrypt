0264c8d7826c8631675919d7e1360883f6c88d56
Shovan04 <shovanm50@gmail.com>
2024-08-06 10:49:38 +0530

    fix issue

--- novacrypt.cpp
+++ novacrypt.cpp
@@ -3,24 +3,28 @@
 NovaCrypt::NovaCrypt(int secureKey, int salt)
     : secureKey(secureKey), salt(salt == 0 ? 9 : salt) {}
 
-std::string NovaCrypt::encrypt(const std::string &text)
+std::string NovaCrypt::encrypt(const std::string &content)
 {
-    std::string encryptedString;
-    for (char c : text)
+    std::string encrypted_content;
+    for (char c : content)
     {
-        int genSpecCode = ((static_cast<int>(c) + (secureKey * salt)) + salt);
-        encryptedString += static_cast<char>(genSpecCode%256);
+        int genSpecCode = (static_cast<int>(c) + (secureKey * salt) + salt) % 256;
+        encrypted_content += static_cast<char>(genSpecCode);
     }
-    return encryptedString;
+    return encrypted_content;
 }
 
-std::string NovaCrypt::decrypt(const std::string &text)
+std::string NovaCrypt::decrypt(const std::string &encrypted_content)
 {
-    std::string deCryptedString;
-
-    for (char c : text)
+    std::string decrypted_content;
+    for (char c : encrypted_content)
     {
-        deCryptedString += ((static_cast<int>(c) - salt) - (secureKey * salt));
+        int decSpecCode = (static_cast<int>(c) - salt - (secureKey * salt)) % 256;
+        if (decSpecCode < 0)
+        {
+            decSpecCode += 256;
+        }
+        decrypted_content += static_cast<char>(decSpecCode);
     }
-    return deCryptedString;
+    return decrypted_content;
 }
